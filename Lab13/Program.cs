33973585f217172f1cdc7a507df5b198f9e042f0
Kendra Hansford-Bellamy <83508389+khansfor@users.noreply.github.com>
2022-05-18 14:52:22 -0400

    Add files via upload

---
+++ Lab13/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab13
+{
+
+    class Baby
+    {
+        int time;
+        string name;
+
+        public Baby(string name)
+        {
+            this.name = name;
+            Random r1 = new Random();
+
+            time = r1.Next(5000);
+        }
+
+        public void Run()
+        {
+            try
+            {
+                Console.WriteLine("My name is " + name + " I am going to sleep for " + time + "ms");
+                Thread.Sleep(time);
+
+                Console.WriteLine("My name is " + name + ", I am awake, feed me!!!");
+            }
+
+            catch(ThreadInterruptedException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baby b1 = new Baby("Noah");
+            Baby b2 = new Baby("Olivia");
+            Baby b3 = new Baby("Liam");
+            Baby b4 = new Baby("Emma");
+            Baby b5 = new Baby("Amelia");
+
+            Thread t1 = new Thread(b1.Run);
+            Thread t2 = new Thread(b2.Run);
+            Thread t3 = new Thread(b3.Run);
+            Thread t4 = new Thread(b4.Run);
+            Thread t5 = new Thread(b5.Run);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+        }
+    }
+}
